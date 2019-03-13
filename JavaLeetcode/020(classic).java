05ba0f5ba5e17a6b031055c49f6e3847b2dd3397
gforc <gforc@126.com>
2019-03-13 18:33:09 +0800

    Update 020(classic).java

--- JavaLeetcode/020(classic).java
+++ JavaLeetcode/020(classic).java
@@ -5,8 +5,9 @@
         for (char c : s.toCharArray()) {
             if (c == '(' || c == '[' || c == '{') {
                 stack[top++] = c; 
-            } else if (c == ')' && stack[--top] != '(') { ####先匹配c == ')'，如果匹配成功，再匹配stack[--top] != '('，无论是否匹配上，top都自己减1；如果c == ')'没有匹配成功，直接跳到下一个if语句，不在匹配&&后面的，所有top不会减1.
-                return false;
+            } else if (c == ')' && stack[--top] != '(') { //先匹配c == ')'，如果匹配成功，再匹配stack[--top] != '('，无论是否匹配上，top都自己减1；
+                                                          //如果c == ')'没有匹配成功，直接跳到下一个if语句，不在匹配&&后面的，所有top不会减1.
+                return false;                                   
             } else if (c == ']' && stack[--top] != '[') {
                 return false;
             } else if (c == '}' && stack[--top] != '{') {
